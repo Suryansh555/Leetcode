dc460717e89a6ab2bec25724f7772cf6575bf2e7
Suryansh555 <suryansh555@yahoo.in>
2022-09-22 02:49:03 +0530

    More Problems Solved

---
+++ LeetCode 75/Day 7/Binary Search.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0 ;
+        int high = nums.size() - 1 ;
+        int ans = -1 ;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target){
+                ans = mid ;
+                break ;
+            }
+            if(nums[mid] < target){
+                low = mid + 1 ;
+            }
+            else{
+                high = mid - 1 ;
+            }
+        }
+        return ans ;
+    }
+};
