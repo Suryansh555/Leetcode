2bc56d2624298a4744f492a724516bb0aeecd544
Suryansh555 <suryansh555@yahoo.in>
2022-09-23 13:39:50 +0530

    More Problems Solved

---
+++ PreorderTraversal.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+ struct TreeNode {
+     int val;
+    TreeNode *left;
+    TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution {
+public:
+    vector<int> inorder1;
+    
+    void inorder(TreeNode* root){
+        if(!root){
+            return ;
+        }
+        inorder1.push_back(root-> val);
+        inorder(root -> left);
+        
+        inorder(root->right);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        inorder(root);
+        return inorder1 ;
+    }
+};
