f09f45ce1d97e0bb9a9f61adce213572644a69a1
Suryansh555 <suryansh555@yahoo.in>
2022-10-05 00:31:38 +0530

    More Problems Solved

---
+++ Add Digits.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int addDigits(int num) {
+      int sum=0;
+        while(num>9)
+        {
+            while(num)
+            {
+                sum+=(num%10);
+                num/=10;
+            }
+            num=sum;
+            sum=0;
+        }
+        return num;
+    }
+};
