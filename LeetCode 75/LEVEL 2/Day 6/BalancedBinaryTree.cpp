1f7316242b38609631e316baa5b114e0559ddf80
Suryansh555 <suryansh555@yahoo.in>
2022-10-05 06:26:51 +0530

    More Problems Solved

---
+++ LeetCode 75/LEVEL 2/Day 6/BalancedBinaryTree.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution {
+public:
+        int dfs(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int left= dfs(root->left);
+        int right= dfs(root->right);
+        
+        if(left==-1 || right==-1 || abs(left-right)>1){
+            return -1;
+        }
+        
+        return max(left,right)+1;
+    }
+    
+    bool isBalanced(TreeNode* root) {
+        
+        if(dfs(root)==-1)
+            return 0;
+        return 1;
+    }
+};
