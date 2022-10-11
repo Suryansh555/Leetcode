23f0ffd2cd76184577485d08d00c1d9a51daae4f
Suryansh555 <suryansh555@yahoo.in>
2022-10-11 06:24:41 +0530

    Added More questions

---
+++ LeetCode 75/LEVEL 2/Day 12/House Robber.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std ;
+class Solution {
+public:
+    int dp[101][40005];
+    int helper(vector<int>&nums,int idx,int robbed){
+        if(idx >= nums.size()){
+            return robbed;
+        }
+        if(dp[idx][robbed]!=-1){
+            return dp[idx][robbed];
+        }
+        int rob = helper(nums,idx+2,robbed+nums[idx]);
+        int notrob = helper(nums,idx+1,robbed);
+        int ans = max(rob,notrob);
+        
+        return dp[idx][robbed] = ans;
+    }
+    
+    int rob(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        return helper(nums,0,0);
+    }
+};
