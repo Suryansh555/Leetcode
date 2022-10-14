bdc6ce45422b0c4eed87dfddd1c1d3dd86122074
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-10-14 07:12:00 +0530

    Added More questions

---
+++ LeetCode 75/LEVEL 2/Day 15/Symmetric Tree.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+
+  struct TreeNode {
+      int val;
+     TreeNode *left;
+      TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+public:
+    bool is_same(TreeNode *root1, TreeNode *root2){
+        if(!root1 && !root2) return true;
+        if((!root1 && root2) || (root1 && !root2)) return false;
+        
+        if(root1->val == root2->val) return is_same(root1->left , root2->right) && is_same(root1->right , root2->left);
+        else return false;
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        return is_same(root->left, root->right);
+    }
+};
+/* If you like it please upvote */
