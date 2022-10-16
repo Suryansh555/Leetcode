6a591de1dc9c1256ea7b12ee6ac6105bff39c096
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-10-17 00:58:13 +0530

    Added More questions

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int i = 0 ; i < n ; i++){
+            mp[nums[i]]++;
+            if(mp[nums[i]] > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
