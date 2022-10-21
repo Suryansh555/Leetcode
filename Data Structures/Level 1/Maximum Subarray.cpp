538c1b827502dfd027dc4a7964ae0884da9bf2f4
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-10-21 09:22:54 +0530

    Added New Datastructure

---
+++ Data Structures/Level 1/Maximum Subarray.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curMax = 0, maxTillNow = INT_MIN;
+        for(auto c : nums)
+            curMax = max(c, curMax + c),
+            maxTillNow = max(maxTillNow, curMax);
+        return maxTillNow;
+    }
+};
