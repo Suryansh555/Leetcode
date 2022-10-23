1ff6733d66f102c966e98707393347a79edd60fa
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-10-23 07:49:08 +0530

    Added More questions

---
+++ Programming Skills/Level 2/Day 2/Repeated SubString Pattern.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int size=s.size(),i=0,j=size-1;
+        string str=s;
+        str+=s;
+        str.pop_back();
+        str.erase(str.begin());
+        while(j<str.size()){
+            string g=str.substr(i,j-i+1); //(j-i+1) helps to calculate the length of the substring from index i to j
+            if(g==s)
+                return true;
+            i++;j++;
+        }
+        return false;
+    }
+};
