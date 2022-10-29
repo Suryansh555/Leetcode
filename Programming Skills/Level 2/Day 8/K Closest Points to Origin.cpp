86b55bd55b36f092bb139ea145fa5f47e3300d2e
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-10-29 10:45:36 +0530

    Added More questions

---
+++ Programming Skills/Level 2/Day 8/K Closest Points to Origin.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std ; 
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> v;
+        priority_queue<pair<int,pair<int,int>>> pq;
+        for(int i=0;i<points.size();i++){
+            int sq = points[i][0]*points[i][0]  + points[i][1]*points[i][1];
+            pq.push({sq,{points[i][0],points[i][1]}});
+        }
+        int n = points.size()-k;
+        
+        while(n--){
+            pq.pop();
+        }
+        while(!pq.empty()){
+            pair<int,pair<int,int>> p = pq.top();
+            pq.pop();
+            vector<int> ans = {p.second.first,p.second.second};
+            v.push_back(ans);
+        }
+        
+        return v;
+    }
+};
