bc67222350cf56010918167c5659ed1f1a636ed2
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-11-01 00:08:15 +0530

    Added More questions

---
+++ SQRT(X).cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x==0) return 0;
+        int left{1}, right{x}, mid{x/2};
+        while (left<=right) {
+            if (mid==0) break;
+            
+            if (x/mid < mid) {
+                right=mid-1;
+            }
+            
+            else if (x/mid > mid) {
+                if (x/(mid+1) < mid+1) return mid;
+                else left = mid+1;
+            }
+
+            else return mid;
+
+            mid=left + (right-left)/2;
+        }
+        return 1;
+    }
+};
