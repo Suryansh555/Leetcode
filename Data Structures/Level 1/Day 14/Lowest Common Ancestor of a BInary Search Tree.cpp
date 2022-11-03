418f0d01c80a6a46ce94be0daf7ab10ddf21b7b7
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-11-03 10:36:06 +0530

    Added More questions

---
+++ Data Structures/Level 1/Day 14/Lowest Common Ancestor of a BInary Search Tree.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ 
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root){
+            return root;
+        }
+        if(root == p || root == q){
+            return root ;
+        }
+        
+        if(p->val < root -> val && q -> val > root -> val){
+            return root;
+        }
+        else if(p -> val < root -> val && q -> val <root -> val ){
+  return lowestCommonAncestor(root->left,p,q);
+        }
+        else if( p-> val > root -> val && q -> val < root -> val ){
+            return root;
+        }
+        else if(p -> val > root -> val && q -> val > root -> val){
+ return     lowestCommonAncestor(root->right,p,q);
+        }
+        else{
+            return root ;
+        }
+            
+            
+        }
+        
+    
+};
---
+++ Data Structures/Level 1/Day 14/Validate Binary Search Tree.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+  bool isValidBST(TreeNode* node, long mn = (long) INT_MIN - 1, long mx = (long) INT_MAX + 1) {
+    if (node->val <= mn || node->val >= mx) return false;
+    
+    bool left = true, right = true;
+    if (node->left) left = isValidBST(node->left, mn, min(mx, (long) node->val));
+    if (node->right) right = isValidBST(node->right, max(mn, (long) node->val), mx);
+    return left && right;
+  }
+};
