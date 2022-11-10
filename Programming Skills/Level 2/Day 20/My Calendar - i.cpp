09e1df745c2684272b840ad9e7dadbaa977f623b
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-11-10 16:40:16 +0530

    Added More questions

---
+++ Programming Skills/Level 2/Day 20/My Calendar - i.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyCalendar {
+    map<int, int> mp;   
+public:  
+    bool book(int start, int end) {
+
+        auto itr = mp.upper_bound(start);
+        if(itr == mp.end() || end<= itr->second) {
+            mp[end]=start;
+            return true;
+        } 
+        else  return false;
+    }
+};
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
