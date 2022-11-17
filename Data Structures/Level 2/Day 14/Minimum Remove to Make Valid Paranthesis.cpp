604fe03990bfa5c69d0161ff54989e3ccd1eb369
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-11-17 07:33:43 +0530

    Added More Problems

---
+++ Data Structures/Level 2/Day 14/Minimum Remove to Make Valid Paranthesis.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                count++;
+            }
+            else if(s[i]==')'){
+                if(count>0) count--;
+                else s[i]='*';
+            }
+        }
+        count=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==')'){
+                count++;
+            }
+            else if(s[i]=='('){
+                if(count>0) count--;
+                else s[i]='*';
+            }
+        }
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='*') ans+=s[i];
+        }
+        return ans;
+    }
+};
