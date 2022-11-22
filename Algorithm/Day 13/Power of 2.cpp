007190786e22d0279fa9caebace57d8ff82b92b0
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-11-22 23:47:16 +0530

    Added More Problems

---
+++ Algorithm/Day 13/Power of 2.cpp
@@ -0,0 +1,11 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        return ceil(log2(n)) == floor(log2(n));
+    }
+};
