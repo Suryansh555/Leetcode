82a8728f2693290eeb577d9660d28a9b46935482
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-11-23 21:38:25 +0530

    Added More Problems

---
+++ Data Structures/Level 2/Day 20/Top K Frequent Elements.cpp
@@ -0,0 +1,35 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define pi pair<int,int>
+class mycomparator{
+    public:
+        bool operator() (const pi &a1 , const pi &a2){
+
+            return a1.first < a2.first ;
+        }
+};
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> mp ;
+        int n ;
+        n = nums.size();
+        for(int i = 0 ; i < n ; i++){
+            mp[nums[i]]++;
+        }
+        priority_queue<pi,vector<pi>,mycomparator> pq;
+        for(auto i : mp){
+            pq.push(pair<int,int>(i.second , i.first));
+        }
+        int a = 0;
+        vector<int> res ;
+        while(a != k && pq.size() != 0){
+            res.push_back(pq.top().second);
+            pq.pop();
+            a++;
+        }
+        return res ;
+    }
+};
