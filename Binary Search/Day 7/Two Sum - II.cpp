738e0e563bc7a83c49607406595bd5afec48a085
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-12-01 08:41:06 +0530

    Added More Problems

---
+++ Binary Search/Day 7/Two Sum - II.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size() - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum > target) {
+                --r;
+            } else if (sum < target) {
+                ++l;
+            } else {
+                return {l + 1, r + 1};
+            }
+        }
+        return {0, 0};
+    }
+};
