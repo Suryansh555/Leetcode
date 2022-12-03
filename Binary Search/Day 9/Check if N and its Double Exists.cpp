2ece2af0074dbaa15b47011be5acdfdf26b06530
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-12-03 21:57:03 +0530

    Added More Problems

---
+++ Binary Search/Day 9/Check if N and its Double Exists.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+		
+        bool ans = false;
+		
+        for(int i=0;i<arr.size();++i){		
+            for(int j=0;j<arr.size();++j){
+                if(i!=j && ((arr[i]%2==0 && arr[i]==2*arr[j]) || (arr[i]%2==0 && arr[i]/2==arr[j]))){
+				
+                    ans = true;
+                    break;
+                }
+            }
+        }
+         return ans;
+    }
+};
---
+++ Binary Search/Day 9/The Kth Weakest Row.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+    set<pair<int, int>> m;
+    for (auto i = 0; i < mat.size(); ++i) {
+        auto p = accumulate(begin(mat[i]), end(mat[i]), 0);
+        m.insert({p, i});
+    }
+    vector<int> res;
+    for (auto it = begin(m); k > 0; ++it, --k)
+        res.push_back(it->second);
+    return res;
+}
+};
