b166ebcb30c1cb21b39c408e56651bca7a269c55
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-12-04 17:25:02 +0530

    Added More Problems

--- Algorithm/Level 2/Day 10/Combination Sum - II.cpp
+++ Algorithm/Level 2/Day 10/Combination Sum - II.cpp
@@ -5,28 +5,22 @@ class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        return find(candidates, 0, target);
+        vector<vector<int>>res;
+        vector<int>path;
+        DFS(res, candidates, 0, 0, target, path);
+        return res;
     }
     
-    vector<vector<int>> find(vector<int>& candidates,int begin, int target){
-        vector<vector<int>> result;
-        
-        for(int i = begin; i < candidates.size(); ++i){
-            if(target > candidates[i]){            
-                vector<vector<int>> temp = find(candidates, i+1, target - candidates[i]);
-                for(int j = 0; j < temp.size(); j++){
-                    temp[j].push_back(candidates[i]);
-                    result.push_back(temp[j]);
-                }
-            }else if(candidates[i] == target){
-                result.push_back({candidates[i]});
-            }else break;
-            
-            while(candidates[i+1] == candidates[i]){
-                i++;
-            }
+    void DFS(vector<vector<int>>& res, vector<int>& candidates, int pos, int sum, int target, vector<int>& path){
+        if(sum >= target){
+            if(sum == target) res.push_back(path);
+            return;
         }
-        
-        return result;
+        for(int i = pos; i < candidates.size(); i++){
+            if(i != pos && candidates[i] == candidates[i - 1]) continue;
+            path.push_back(candidates[i]);
+            DFS(res, candidates, i + 1, sum + candidates[i], target, path);
+            path.pop_back();
+        }
     }
 };
