319bbe18eea10eb8d32b7d870edc648b2d0aa6b3
Suryansh Shrivastava <suryansh555@yahoo.in>
2022-12-07 18:23:10 +0530

    Added More Problems

---
+++ Algorithm/Level 2/Day 13/Unique Paths.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int cache[101][101] = {}; // for memoization
+    int uniquePaths(int m, int n) {
+        if(m < 1 || n < 1) return 0; 
+        if(cache[m][n]) return cache[m][n];
+        if(m == 1 && n == 1) return 1;
+        return cache[m][n] = uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    }
+};;
