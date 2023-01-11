804070220a2e00fd0d7eaaaf97c65bcbd9455619
Suryansh Shrivastava <suryansh555@yahoo.in>
2023-01-11 17:20:09 +0530

    Added More Problems

---
+++ Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0;
+        for(int i=0;i<operations.size();i++){
+            if(operations[i]=="++X" || operations[i]=="X++") 
+            x++;
+            else
+                x--;
+        }
+    return x;    
+    
+    }
+
+};
