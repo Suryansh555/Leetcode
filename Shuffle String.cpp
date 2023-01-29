ccbf61446eb393e74d1025e8239f4e02e01fcc38
Suryansh Shrivastava <suryansh555@yahoo.in>
2023-01-29 21:21:34 +0530

    Added More Problems

---
+++ Shuffle String.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        
+        string str=s;
+
+        for(int i=0;i<s.size();i++){
+            str[indices[i]]=s[i];
+        }
+        return str;
+    }
+};
