814a0da18093f492157326f76a937a139aab2ef4
Suryansh Shrivastava <suryansh555@yahoo.in>
2023-02-09 16:35:49 +0530

    Added More Problems

---
+++ Robot Return to Origin.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int row = 0, col = 0;
+        for (int i=0; i<moves.length(); i++)
+        {
+            if (moves[i] == 'L')
+                col--;
+            else if (moves[i] == 'R')
+                col++;
+            else if (moves[i] == 'U')
+                row--;
+            else
+                row++;
+        }
+
+        if (row == 0 && col == 0)
+            return 1;
+        else
+            return 0;
+        
+    }
+};
