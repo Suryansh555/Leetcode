80f12676c8b018aa118c4b839e076b53d80aae75
Suryansh Shrivastava <suryansh555@yahoo.in>
2023-02-10 15:55:58 +0530

    Added More Problems

---
+++ Delete columns to make Sorted.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        
+   int ans= 0;
+  
+   for(int col= 0; col<strs[0].size();col++){
+
+         for(int row= 1; row<strs.size();row++){
+
+
+               if(strs[row-1][col] > strs[row][col]){
+
+                   ans++;
+                   break;
+               }
+         }
+   }
+
+
+   return ans;
+    }
+};
